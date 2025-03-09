98d19039078b95fd0bf710b3f02bd6c92c8554d6
leitershery <leitershery@gmail.com>
2025-03-09 21:12:50 +0000

    Add 4.c

---
+++ src/4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+  int age = 25;
+  if (age >= 18) {
+    printf("You are eligible to vote.\n");
+  } else {
+    printf("You are not eligible to vote yet.\n");
+  }
+  return 0;
+}
