bf82c102ded4f0f343e91a7686732804021499eb
leitershery <leitershery@gmail.com>
2025-03-15 20:48:30 +0000

    Add 10.c

---
+++ src/10.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void) {
+    int x = 5;
+    if (x > 0) {
+        printf("X is positive\n");
+    } else {
+        printf("X is not positive\n");
+    }
+    return 0;
+}
