6f555b603bd1ea7645c051453c702917d613125a
leitershery <leitershery@gmail.com>
2025-03-23 09:28:34 +0000

    Add 17.c

---
+++ src/17.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int x = 5;
+    if (x > 0) {
+        printf("The number is positive.\n");
+    } else {
+        printf("The number is not positive.\n");
+    }
+    return 0;
+}
