02d4089e9e356a4e75e50d6f6f2dc884777f9c9c
leitershery <leitershery@gmail.com>
2025-04-26 11:38:22 +0000

    Add 37.c

---
+++ src/37.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main() {
+    int i;
+    
+    printf("Hello, world!\n");
+    printf("This is a C program.\n");
+    printf("I'm written by Qwen for your project.\n");
+}
